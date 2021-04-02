1a2fad2cd19884f255699336ffe36c50dfe9878b
a17private <a17matte@gmail.com>
2021-04-02 23:43:49 +0100

    Addition of 'for' loop to print a list of Pokémon and their height.

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,18 @@ name: 'Pikachu',
 height: 0.4,
 types: 'electric'},
 ];
+
+let pokemon = ['Charizard(height:1.7)', 'Squirtle(height: 0.5)', 'Pikachu(height: 0.4)'];
+for (let i = 0; i <= pokemonList.length; i++)
+{
+if (pokemonList[i].height <2) && pokemonList[i].height >1)
+}
+{
+document.write(pokemonList[i].name + "Wow, that's big!");
+}
+else if (pokemonList[i].height < 1) && pokemonList[i].height >=0.5)
+{
+(document.write(pokemonList[i].name);
+}
+else (document.write(pokemonList[i].name);
+}
