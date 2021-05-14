4cf185edcf3501eb5812b1b0538a001377246723
a17labs <79963413+a17private@users.noreply.github.com>
2021-05-14 17:39:02 +0100

    Newest changes 1.10

--- js/scripts.js
+++ js/scripts.js
@@ -2,33 +2,31 @@ let pokemonRepository = (function () {
 let modalContainer = $("#modal-container");
 let pokemonList = [];
 let apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
-
 function getAll() {
   return pokemonList;
 }
-
 function add(pokemon){
   pokemonList.push(pokemon);
 }
-
 function addListItem (pokemon){
-  let listPokemon = $('pokemon-list-item')[0]
-  listPokemon.parent();
-  const name = (pokemon.name);
-  let listItem = $('<li class="pokemon-list-item" class="pokemon-list-item-action" >name[0].toUpperCase();</li>');
-  let button = $('<div class="button" class="btn" class="btn-dark" class="btn-block" class="mb-2">name[0].toUpperCase();</div>');
-   $('button').attr("data-target","#pokemonModal","data-toggle","modal")
-   $('listPokemon').append(listItem);
-   $('listItem').append(button);
+  let listPokemon = $('.pokemon-list-item');
+  let listItem = $('<li></li>');
+  listItem.addClass('pokemon-list-item pokemon-list-item-action');
+  let button = $('<button></button>');
+  button.addClass('btn btn-dark btn-block');
+   button.attr({"data-target":"#pokemonModal","data-toggle":"modal"})
+  button.text(pokemon.name);
+   listItem.appendTo(listPokemon);
+   button.appendTo(listItem);
     //add event listener to our items button that show details
-   button.on("click", function(event){
-   showDetails(pokemon);
+   button.on("click", function(){
+    showDetails(pokemon);
   });
 }
-
-function loadList () {
-  $.ajax('apiUrl', { dataType: 'json'}).then(function(response) {
-  console.log(responseJSON);
+function loadList() {
+  /*return*/
+  $.ajax(apiUrl, { dataType: 'json'}).then(function(response) {
+  return response;
   })
   .then(function(json) {
   json.results.forEach(function(item) {
@@ -42,11 +40,10 @@ function loadList () {
     console.log('Caught an error:' + err.statusText);
   })
   }
-
 function loadDetails(item) {
   let url = item.detailsUrl;
-      $('url', { dataType: 'json'}).then(function (response) {
-      console.log(responseJSON);
+      return $.ajax(url, { dataType: 'json'}).then(function (response) {
+        return response;
     })
       .then(function (details) {
       // Now we add the details to the item
@@ -60,41 +57,30 @@ function loadDetails(item) {
 }
 // show the details of pokemon
 function showDetails(pokemon) {
-  pokemonRepository.loadDetails(pokemon).then(function() {
+  loadDetails(pokemon).then(function() {
     let modalBody = $('.modal-body');
     let modalTitle = $('.modal-title');
-
     modalTitle.empty();
     modalBody.empty();
-
  // Create eLement for name within the modal content.
     let nameElement = $('<h1>' + pokemon.name + '</h1>');
     modalTitle.appendChild(nameElement);
-
   // Create eLement for image within the modal content.
     let imageElementFront = $("<img class='modal-img' style = 'width:50%'>");
     imageElementFront.attr("src", pokemon.imageUrl);
     modalBody.appendChild(imageElementFront);
-
   // Create element for weight within modal content.
     let heightElement = $("<p>"+"height : " + pokemon.height + "</p>");
     modalBody.appendChild(heightElement);
-
   // Create element for type within the modal content.
     let typesElement = $("</p>" + "types : " + pokemon.types + "</p>");
-
-
     modalTitle.append(nameElement);
     modalBody.append(imageElementFront);
     modalBody.append(heightElement);
     modalBody.append(typesElement);
-
-
      $('#pokemonModal').modal('toggle');
   });
 }
-
-
 return{
   add: add,
   getAll: getAll,
@@ -104,7 +90,6 @@ return{
   showDetails: showDetails
   };
 })();
-
 pokemonRepository.loadList().then(function() {
   pokemonRepository.getAll().forEach(function(pokemon) {
     pokemonRepository.addListItem(pokemon);
